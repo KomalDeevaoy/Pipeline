ac5eda85c5c533e4c753d3446b205b6af8d63fcb
KomalDeevaroy <deevaroykomal@gmail.com>
2026-06-30 13:55:55 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
-  public static void main(String[] args){
-    System.out.println("Hello Guys!");
+  public void main(String[] args){
+    System.out.println("Hello Guys!")
     System.out.println("wELCOME TO VBIT");
   }
 }
